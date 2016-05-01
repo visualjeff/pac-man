bf82769385b51ba2f89721146c2f111c6c2a03c1
Jeffrey Gilbert <visualjeff@linux.com>
2016-04-30 21:08:13 -0700

    at chapter 12

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -8,22 +8,26 @@ default Ember.Component.extend(KeyboardShortcuts, {
         let ctx = canvas.getContext("2d");
         return ctx;
     }),
+    score: 0,
+    levelNumber: 1,
     x: 1,
     y: 2,
     squareSize: 40,
     //0 is a blankspace 
     //1 is a wall
+    //2 is a pellet
     grid: [
-        [0, 0, 0, 0, 0, 0, 0, 1],
-        [0, 1, 0, 1, 0, 0, 0, 1],
-        [0, 0, 1, 0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 1],
+        [2, 2, 2, 2, 2, 2, 2, 1],
+        [2, 1, 2, 1, 2, 2, 2, 1],
+        [2, 2, 1, 2, 2, 2, 2, 1],
+        [2, 2, 2, 2, 2, 2, 2, 1],
+        [2, 2, 2, 2, 2, 2, 2, 1],
+        [1, 2, 2, 2, 2, 2, 2, 1],
     ],
+    isMoving: false,
+    direction: 'down',
     didInsertElement() {
-        this.drawWalls();
-        this.drawCircle();
+        this.movementLoop();
     },
     screenPixelWidth: Ember.computed(function() {
         return this.get('screenWidth') * this.get('squareSize');
@@ -31,35 +35,50 @@ default Ember.Component.extend(KeyboardShortcuts, {
     screenPixelHeight: Ember.computed(function() {
         return this.get('screenHeight') * this.get('squareSize');
     }),
-    drawWalls() {
-        let squareSize = this.get('squareSize');
+    drawWall(x, y) {
         let ctx = this.get('ctx');
+        let squareSize = this.get('squareSize');
         ctx.fillStyle = '#000';
+        ctx.fillRect(x * squareSize, y * squareSize, squareSize, squareSize);
+    },
+    drawPellet(x, y) {
+        let radiusDivisor = 6;
+        this.drawCircle(x, y, radiusDivisor, 'stopped');
+    },
+    drawGrid() {
         let grid = this.get('grid');
-        grid.forEach(function(row, rowIndex) {
-            row.forEach(function(cell, columnIndex) {
+        //NOTE the use of the fat array below.  Facilitates scope of this.
+        grid.forEach((row, rowIndex) => {
+            row.forEach((cell, columnIndex) => {
                 if (cell == 1) {
-                    ctx.fillRect(columnIndex * squareSize, rowIndex * squareSize, squareSize, squareSize);
+                    this.drawWall(columnIndex, rowIndex);
+                }
+                if (cell == 2) {
+                    this.drawPellet(columnIndex, rowIndex);
                 }
             });
         });
     },
-    drawCircle() {
-        let ctx = this.get('ctx');
+    drawPac() {
         let x = this.get('x');
         let y = this.get('y');
-
+        let radiusDivisor = 2;
+        this.drawCircle(x, y, radiusDivisor, this.get('direction'));
+    },
+    drawCircle(x, y, radiusDivisor, direction) {
+        let ctx = this.get('ctx');
         let squareSize = this.get('squareSize');
-        let pixelX = (x + 1 / 2) * squareSize;
-        let pixelY = (y + 1 / 2) * squareSize;
-
-        //console.log(`pixelX: ${pixelX}, pixelY: ${pixelY}`);
-
+        let pixelX = (x + 1 / 2 + this.offsetFor('x', direction)) * squareSize;
+        let pixelY = (y + 1 / 2 + this.offsetFor('y', direction)) * squareSize;
         ctx.fillStyle = '#000';
         ctx.beginPath();
-        ctx.arc(pixelX, pixelY, squareSize / 2, 0, Math.PI * 2, false);
+        ctx.arc(pixelX, pixelY, squareSize / radiusDivisor, 0, Math.PI * 2, false);
         ctx.closePath();
         ctx.fill();
+    },
+    offsetFor(coordinate, direction) {
+        let frameRatio = this.get('frameCycle') / this.get('framesPerMovement');
+        return this.get(`directions.${direction}.${coordinate}`) * frameRatio;
     },
     screenWidth: Ember.computed(function() {
         return this.get('grid.firstObject.length');
@@ -94,26 +113,132 @@ default Ember.Component.extend(KeyboardShortcuts, {
     movePacMan(direction, amount) {
         this.incrementProperty(direction, amount);
 
-        if (this.collidedWithBorder() || this.collidedWithGrid()) {
-            this.decrementProperty(direction, amount);
+        if (this.get('isMoving') || this.pathBlockedInDirection(direction)) {
+            // do nothing, just wait it out
+        } else {
+            this.set('direction', direction);
+            this.set('isMoving', true);
+            this.movementLoop();
         }
-
-        this.clearScreen();
-        this.drawWalls();
-        this.drawCircle();
     },
-    keyboardShortcuts: {
-        up: function() {
-            this.movePacMan('y', -1);
-        },
-        down: function() {
-            this.movePacMan('y', 1);
-        },
-        left: function() {
-            this.movePacMan('x', -1);
-        },
-        right: function() {
-            this.movePacMan('x', 1);
+    frameCycle: 1,
+    framesPerMovement: 30,
+    movementLoop() {
+        if (this.get('frameCycle') == this.get('framesPerMovement')) {
+            let direction = this.get('direction');
+            this.set('x', this.nextCoordinate('x', direction));
+            this.set('y', this.nextCoordinate('y', direction));
+
+            this.set('frameCycle', 1);
+            this.processAnyPellets();
+            this.changePacDirection();
+        } else if (this.get('direction') == 'stopped') {
+            this.changePacDirection();
+        } else {
+            this.incrementProperty('frameCycle');
         }
+        this.clearScreen();
+        this.drawGrid();
+        this.drawPac();
+
+        Ember.run.later(this, this.movementLoop, 1000 / 60);
+    },
+    changePacDirection() {
+        let intent = this.get('intent');
+        if (this.pathBlockedInDirection(intent)) {
+            this.set('direction', 'stopped');
+        } else {
+            this.set('direction', intent);
+        }
+    },
+
+
+    nextCoordinate(coordinate, direction) {
+        let nextCoordinate = this.get(coordinate) + this.get(`directions.${direction}.${coordinate}`);
+        //console.log(`nextCoordinate: ${nextCoordinate}`);
+        return nextCoordinate;
+    },
+    pathBlockedInDirection(direction) {
+        let cellTypeInDirection = this.cellTypeInDirection(direction);
+        return Ember.isEmpty(cellTypeInDirection) || cellTypeInDirection === 1;
+    },
+    cellTypeInDirection(direction) {
+        let nextX = this.nextCoordinate('x', direction);
+        let nextY = this.nextCoordinate('y', direction);
+        return this.get(`grid.${nextY}.${nextX}`);
+    },
+    processAnyPellets() {
+        let x = this.get('x');
+        let y = this.get('y');
+        let grid = this.get('grid');
+        if (grid[y][x] == 2) {
+            grid[y][x] = 0;
+            this.incrementProperty('score');
+            if (this.levelComplete()) {
+                this.incrementProperty('levelNumber');
+                this.restartLevel();
+            }
+        }
+    },
+    levelComplete() {
+        let hasPelletsLeft = false;
+        let grid = this.get('grid');
+        grid.forEach((row) => {
+            row.forEach((cell) => {
+                if (cell == 2) {
+                    hasPelletsLeft = true;
+                }
+            });
+        });
+        return !hasPelletsLeft;
+    },
+    restartLevel: function() {
+        this.set('x', 0);
+        this.set('y', 0);
+        let grid = this.get('grid');
+        grid.forEach((row, rowIndex) => {
+            row.forEach((cell, columnIndex) => {
+                if (cell == 0) {
+                    grid[rowIndex][columnIndex] = 2;
+                }
+            });
+        });
+    },
+    directions: {
+        'up': {
+            x: 0,
+            y: -1
+        },
+        'down': {
+            x: 0,
+            y: 1
+        },
+        'left': {
+            x: -1,
+            y: 0
+        },
+        'right': {
+            x: 1,
+            y: 0
+        },
+        'stopped': {
+            x: 0,
+            y: 0
+        }
+    },
+    intent: 'down',
+    keyboardShortcuts: {
+        up() {
+                this.set('intent', 'up');
+            },
+            down() {
+                this.set('intent', 'down');
+            },
+            left() {
+                this.set('intent', 'left');
+            },
+            right() {
+                this.set('intent', 'right');
+            }
     }
 });
